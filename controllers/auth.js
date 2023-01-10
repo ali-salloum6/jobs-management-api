b0d00232626df095a96bdb9158d00b2a594f231a
Ali <ali.salloum.gh@gmail.com>
2023-01-10 12:27:26 +0300

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,36 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+
+const register = async (req, res) => {
+    const user = await User.create({ ...req.body })
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        throw new BadRequestError('Please provide an email and a password')
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnauthenticatedError('Please provide a valid email and password')
+    }
+
+    const isMatch = await user.comparePassword(password)
+
+    if (!isMatch) {
+        throw new UnauthenticatedError('Please provide a valid email and password')
+    }
+
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user: {name: user.name}, token})
+}
+
+module.exports = {
+    register,
+    login,
+}
---
+++ middleware/authentication.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { UnauthenticatedError } = require('../errors')
+
+const authenticationMiddleware = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('No token provided')
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const { userId, name } = decoded
+    req.user = { userId: userId, name: name }
+    next()
+  } catch (error) {
+    throw new UnauthenticatedError('Not authorized to access this route')
+  }
+}
+
+module.exports = authenticationMiddleware
